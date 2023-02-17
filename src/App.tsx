5a07f8977d2db58893cc4046a49b297322d7a2e9
andyvy <thienvy95@gmail.com>
2023-02-17 20:45:34 +0700

    Create base

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import TopRatedPage from './pages/TopRatedPage';
 
-function App() {
+const client = new QueryClient();
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <QueryClientProvider client={client}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/top-rated" element={<TopRatedPage />} />
+      </Routes>
+    </QueryClientProvider>
+  )
 }
 
-export default App;
+export default App
